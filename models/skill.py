667c47b61fe6ef8210d8598199d571a0b5a0b185
Yaroslav <zerowkacp@gmail.com>
2026-06-29 16:02:48 +0300

    Delete old code

--- models/skill.py
+++
@@ -1,7 +0,0 @@
-from typing import Annotated
-from pydantic import BaseModel, Field
-
-class SkillModel(BaseModel):
-    name:Annotated[str|None, Field(max_length=35)] = None
-    desc:Annotated[str|None, Field(max_length=200)] = None
-    weight:Annotated[int|None, Field(ge=0, le=5)] = None
